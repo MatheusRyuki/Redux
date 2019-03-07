1f966bb87039c6e4c69e1cbb5ece4d28f50024d0
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-07 12:48:39 -0300

    Passando e Recebendo dados com ações

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,23 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        };
+    } else if(action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
+    } else if(action.type === 'ADD') {
+        return {
+            counter: state.counter + action.val
+        };
+    } else if(action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.val
+        };
+    };
     return state;
 };
 
