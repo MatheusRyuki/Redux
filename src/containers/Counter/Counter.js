e766b82da04b243e1f14a1373b21d1ba7786b102
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-10 15:23:52 -0300

    Actions Introdução

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import { increment } from '../../store/actions/actions';
 
 class Counter extends Component {
   render() {
@@ -43,7 +43,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+  onIncrementCounter: () => dispatch( increment()),
   onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
   onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 5 }),
   onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 5 }),
