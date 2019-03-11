6a66787586ec0ee2ac353420ee0ceda493325608
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-11 09:58:34 -0300

    Switch mais limpo

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,15 +5,18 @@ const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const newArray = state.results.filter(result => result.id !== action.resultId);
+  return updateObject(state, { results: newArray });
+}
+
 const reducer = (state = initialState, action) => {
   // eslint-disable-next-line default-case
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       return updateObject(state, { counter: 0, results: state.results.concat({ id: new Date(), value: action.result }) });
-
-    case actionTypes.DELETE_RESULT:
-      const newArray = state.results.filter(result => result.id !== action.resultId);
-      return updateObject(state, { results: newArray });
+    case actionTypes.DELETE_RESULT:  
+      return deleteResult(state, action);
   }
   return state;
 };
